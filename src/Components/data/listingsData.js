48532e2cd49854bd43a28615ad739a31ef479530
Matt <MVSucharski@gmail.com>
2019-01-10 10:27:18 -0500

    working on setting up files

---
+++ src/Components/data/listingsData.js
@@ -0,0 +1,101 @@
+var listingsData = [
+    {
+        address: '43 heart street',
+        city: 'Farmingdale',
+        state: 'NY',
+        rooms: 2,
+        price: 230000,
+        floorSpace: 2500,
+        extras: [
+            'pool'
+        ],
+        homeType: 'Apartment',
+        image: 'http://ajackpot.com/images/inside-millionaire-homes/data1/images/insidemillionaireshouses15.jpg'
+    },
+    {
+        address: '48 Fallwood road',
+        city: 'Huntington',
+        state: 'NC',
+        rooms: 6,
+        price: 450000,
+        floorSpace: 3400,
+        extras: [
+            'elevator',
+            'gym'
+            
+        ],
+        homeType: 'House',
+        image: 'http://tasteofcountry.com/files/2018/08/spectacular-celebrity-homes.jpg?w=980&q=75'
+    },
+    {
+        address: '30 Oak Avenue',
+        city: 'Bellmore',
+        state: 'NY',
+        rooms: 5,
+        price: 600000,
+        floorSpace: 3200,
+        extras: [
+            'elevator',
+            'pool'
+        ],
+        homeType: 'House',
+        image: 'https://lentinemarine.com/wp-content/uploads/simple-house-painting-inspirations-one-total_482229.jpg'
+    },
+    {
+        address: '90 Broadway Street',
+        city: 'Rockville Center',
+        state: 'NY',
+        rooms: 3,
+        price: 420000,
+        floorSpace: 3500,
+        extras: [
+            'elevator',
+            'pool'
+        ],
+        homeType: 'House',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbREg69bXlEEj_vXOdss3glg4Q_v_0VOf8K46NPcz5YHMcEOJlHg'
+    },
+    {
+        address: '3 Woodward Parkway',
+        city: 'Farmingdale',
+        state: 'NJ',
+        rooms: 4,
+        price: 2800000,
+        floorSpace: 1400,
+        extras: [
+            'pool'
+        ],
+        homeType: 'Condo',
+        image: 'https://bahayofw.com/wp-content/uploads/2017/08/FB_IMG_1503406401235-1.jpg'
+    },
+    {
+        address: '56 Maple Road',
+        city: 'Massapequa',
+        state: 'NY',
+        rooms: 2,
+        price: 180000,
+        floorSpace: 1400,
+        extras: [
+            
+        ],
+        homeType: 'House',
+        image: 'http://erinnsbeauty.com/wp-content/uploads/2018/06/simple-houses-classy-design-.jpg'
+    },
+    {
+        address: '34 Grand Avenue',
+        city: 'Bethpage',
+        state: 'NY',
+        rooms: 6,
+        price: 250000,
+        floorSpace: 1400,
+        extras: [
+            'elevator',
+            'gym',
+            'pool'
+        ],
+        homeType: 'House',
+        image: 'https://i.ytimg.com/vi/Mcp2wgbYzMw/maxresdefault.jpg'
+    }
+]
+
+export default listingsData;
---
+++ src/Components/realEstate.js
@@ -0,0 +1,120 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+import Header from './Header'
+import Listings from './Listings'
+import Filter from './Filter'
+import listingsData from './data/listingsData.js'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Matt',
+      listingsData,
+      cities : "All",
+      homeType : "All",
+      bedrooms : "0",
+      min_price : 0,
+      max_price : 10000000,
+      min_floor_space : 0,
+      max_floor_space : 5000,
+      min_floor_space : 0,
+      max_floor_space : 5000,
+      elevator: false,
+      finished_basement: false,
+      gym: false,
+      swiming_pool: false,
+      filteredData : listingsData,
+      populateFormsData : '',
+    }
+    this.change = this.change.bind(this)
+    this.filteredData = this.filteredData.bind(this)
+    this.populateForms = this.populateForms.bind(this)
+  }
+  change (event) {
+    let name = event.target.name
+    let value = (event.target.type === 'checkbox') ? event.target.checked : event.target.value
+
+    this.setState({
+      [name]: value
+    }, () => {
+      console.log(this.state)
+      this.filteredData()
+    })
+  }
+  filteredData() {
+    
+      var newData = this.state.listingsData.filter((item) => {
+        return item.price >= this.state.min_price && item.price <= this.state.max_price && item.floorSpace >= this.state.min_floor_space && item.floorSpace <= this.state.max_floor_space && item.rooms >= this.state.bedrooms
+      })
+
+      if(this.state.city != "All"){
+        newData = newData.filter((item) => {
+          return item.city == this.state.city
+        })
+      }
+
+      if(this.state.homeTypes != "All"){
+        newData = newData.filter((item) => {
+          return item.homeTypes == this.state.homeTypes
+        })
+      }
+
+      this.setState({
+        filteredData : newData
+        
+      })
+  }
+
+  populateForms() {
+    // city
+    let cities = this.state.listingsData.map((item) => {
+      return item.city
+    })
+    cities = new Set(cities)
+    cities = [...cities]
+
+
+    // types
+    let homeTypes = this.state.listingsData.map((item) => {
+      return item.homeType
+    })
+    homeTypes = new Set(homeTypes)
+    homeTypes = [...homeTypes]
+
+
+    // rooms
+    let bedrooms = this.state.listingsData.map((item) => {
+      return item.rooms
+    })
+    bedrooms = new Set(bedrooms)
+    bedrooms = [...bedrooms]
+
+    this.setState({
+      populateFormsData: {
+        homeTypes,
+        bedrooms,
+        cities
+      }
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
+
+//   render () {
+//     return (<div> 
+//       <Header />
+//       <section id="content-area">
+//       <Filter change={this.change} globalState={this.state}
+//         populateAction={this.populateForms}
+//       />
+//       <Listings listingsData={this.state.filteredData} />
+//       </section>
+//     </div>)
+//   }
+// }
+
+// const app = document.getElementById('app')
+
+// ReactDOM.render(<App />, app)
